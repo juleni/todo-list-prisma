ecde7343cce372aff51caa44859f33bcad5daa55
juleni <69912179+juleni@users.noreply.github.com>
2023-06-26 10:21:21 +0200

    Added buttons and input field

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
 const page = () => {
-  return <div className="text-2xl">NEW</div>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Add new ToDo</h1>
+      </header>
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 
+          outline-none focus-within:border-slate-100"
+        />
+        <div className="flex gap-2 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 
+          focus-within:bg-slate-700 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 
+          focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default page;

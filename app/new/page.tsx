4f3ba0ef9db536ac6b8c3d928a6fdf9b5744490f
juleni <69912179+juleni@users.noreply.github.com>
2023-06-26 10:58:02 +0200

    Added createTodo()

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { prisma } from "../db";
 
-const page = () => {
+async function createTodo(formData: FormData) {
+  "use server";
+  const title = formData.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    console.log("Invalid title");
+    return;
+  }
+  await prisma.todo.create({ data: { title: title, complete: false } });
+  console.log("create New todo");
+  redirect("/");
+}
+
+export default function Page() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Add new ToDo</h1>
       </header>
-      <form className="flex gap-2 flex-col">
+      <form action={createTodo} className="flex gap-2 flex-col">
         <input
           type="text"
           name="title"
+          placeholder="Enter ToDo to add..."
           className="border border-slate-300 bg-transparent rounded px-2 py-1 
           outline-none focus-within:border-slate-100"
         />
@@ -32,6 +47,4 @@ const page = () => {
       </form>
     </>
   );
-};
-
-export default page;
+}

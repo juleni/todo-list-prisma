561ebaccfde74169cf6997a2729bb45f63257a68
juleni <69912179+juleni@users.noreply.github.com>
2023-06-26 10:07:13 +0200

    Added styles for todo title

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -8,7 +8,10 @@ function TodoItem({ id, title, complete }: TodoItemProps) {
   return (
     <li key={id} className="flex gap-1 items-center">
       <input id={id} type="checkbox" className="cursor-pointer peer" />
-      <label htmlFor={id} className="peer-checked:line-through">
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+      >
         {" "}
         {title}{" "}
       </label>
